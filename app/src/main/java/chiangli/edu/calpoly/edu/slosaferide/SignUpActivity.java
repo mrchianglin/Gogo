53ce5e757dac89260d28c732211b6cbb09903047
mrchianglin <jonathanchianglin@live.com>
2017-01-14 17:41:44 -0800

    Implemented UI in SignUpActivity

--- app/src/main/java/chiangli/edu/calpoly/edu/slosaferide/SignUpActivity.java
+++ app/src/main/java/chiangli/edu/calpoly/edu/slosaferide/SignUpActivity.java
@@ -1,7 +1,11 @@
 package chiangli.edu.calpoly.edu.slosaferide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button facebook = (Button) findViewById(R.id.facebook);
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button google = (Button) findViewById(R.id.google);
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        TextView signin = (TextView) findViewById(R.id.SIGNIN);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
